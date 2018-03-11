16e5008eedc5f45da2e73812e131cc9cb9abc436
stefanovualto <stefano.nepa@vualto.com>
2018-03-11 22:52:01 +0000

    update

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,57 @@
 <template>
     <div>
-        <h1>Youpi</h1>
+        <v-list two-line>
+            <template v-for="(config, index) in configs">
+                <v-list-tile :key="config.playerKey">
+                    <v-list-tile-content>
+                        <v-list-tile-title v-html="config.playerKey"></v-list-tile-title>
+                        <button @click="configs.splice(index, 1)">X</button>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </template>
+        </v-list>
+        <v-card-text style="height: 100px; position: relative">
+            <v-form v-model="valid">
+                <v-text-field
+                label="playerKey"
+                v-model="playerKey"
+                required
+                ></v-text-field>
+            </v-form>
+            <v-btn
+                absolute
+                dark
+                fab
+                top
+                right
+                color="pink"
+                @click="addConfig"
+            >
+            <v-icon>add</v-icon>
+            </v-btn>
+        </v-card-text>
     </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Settings extends Vue {
+    public playerKey: string = '';
+
+    public configs: Array<{
+        playerKey: string,
+        vudrmToken?: string,
+        content: Array<{
+            stream: string,
+            label: string,
+        }>,
+    }> = [
+        { playerKey: 'vuplay-demo-app|12150b2f-8468-4eca-bd22-5e8a1af7237f', content: [] },
+    ];
+
+    public addConfig() {
+        this.configs.push({ playerKey: 'vuplay-demo-app|12150b2f-8468-4eca-bd22-5e8a1af7237f', content: [] });
+    }
+}
+</script>
